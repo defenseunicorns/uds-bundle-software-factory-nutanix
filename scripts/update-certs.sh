4a42f08096d9ab75c34b90a3ece579818d3f05b0
ablanchard <andrew.blanchard@leapfrog.ai>
2023-10-06 16:48:11 -0400

    updating keycloak cert

--- scripts/update-certs.sh
+++ scripts/update-certs.sh
@@ -10,6 +10,8 @@
 # remove the existing cert secrets
 kubectl delete secret -n istio-system admin-cert
 kubectl delete secret -n istio-system tenant-cert
+kubectl delete secret -n keycloak keycloak-tlskey
+kubectl delete secret -n keycloak keycloak-tlscert
 
 # create new cert secrets
 kubectl create -n istio-system secret tls admin-cert \
@@ -19,3 +21,9 @@ kubectl create -n istio-system secret tls admin-cert \
 kubectl create -n istio-system secret tls tenant-cert \
   --key=$(git rev-parse --show-toplevel)/scripts/mtsi-bigbang-dev-privkey.pem \
   --cert=$(git rev-parse --show-toplevel)/scripts/mtsi-bigbang-dev-fullchain.pem
+
+kubectl create -n keycloak secret generic keycloak-tlskey --from-file=tls.key=$(git rev-parse --show-toplevel)/scripts/mtsi-bigbang-dev-privkey.pem
+kubectl create -n keycloak secret generic keycloak-tlscert --from-file=tls.crt=$(git rev-parse --show-toplevel)/scripts/mtsi-bigbang-dev-fullchain.pem
+
+# restart keycloak
+kubectl rollout restart statefulset -n keycloak keycloak
