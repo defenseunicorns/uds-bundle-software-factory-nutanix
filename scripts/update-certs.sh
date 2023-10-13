9db48775cbbb410aa2f938efd8bde2057e16ef3e
jacobbmay <134300709+jacobbmay@users.noreply.github.com>
2023-10-13 10:14:24 -0400

    Update cert script and make deploy target to enable choosing a cert and key file  (#4)
    * Update cert script and make deploy target to enable choosing a cert and key file when deploying
    
    * Change block comment in cert script to reflect new params

--- scripts/update-certs.sh
+++ scripts/update-certs.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 ##
 ## This script can be used after a fresh deploy to remove the default (bigbang.dev) cert secret
-##   from the istio-system namespace and create new secrets with the `*.mtsi.bigbang.dev` cert.
+##   from the istio-system namespace and create new secrets with a cert using a custom domain such
+##   as `*.mtsi.bigbang.dev`.
 ##
-## PREREQS:
-##   * Access to mtsi-bigbang-dev-fullchain.pem (contact ablanchard)
-##   * Access to mtsi-bigbang-dev-privkey.pem (contact ablanchard)
+## PARAMS:
+##   * $1 -- relative path to a cert file in pem format (contact ablanchard)
+##   * $2 -- relative path to a key file in pem format (contact ablanchard)
+
+cert=$1
+key=$2
 
 # remove the existing cert secrets
 kubectl delete secret -n istio-system admin-cert
@@ -15,15 +19,15 @@ kubectl delete secret -n keycloak keycloak-tlscert
 
 # create new cert secrets
 kubectl create -n istio-system secret tls admin-cert \
-  --key=$(git rev-parse --show-toplevel)/scripts/mtsi-bigbang-dev-privkey.pem \
-  --cert=$(git rev-parse --show-toplevel)/scripts/mtsi-bigbang-dev-fullchain.pem
+  --key=$(git rev-parse --show-toplevel)/$key \
+  --cert=$(git rev-parse --show-toplevel)/$cert
 
 kubectl create -n istio-system secret tls tenant-cert \
-  --key=$(git rev-parse --show-toplevel)/scripts/mtsi-bigbang-dev-privkey.pem \
-  --cert=$(git rev-parse --show-toplevel)/scripts/mtsi-bigbang-dev-fullchain.pem
+  --key=$(git rev-parse --show-toplevel)/$key \
+  --cert=$(git rev-parse --show-toplevel)/$cert
 
-kubectl create -n keycloak secret generic keycloak-tlskey --from-file=tls.key=$(git rev-parse --show-toplevel)/scripts/mtsi-bigbang-dev-privkey.pem
-kubectl create -n keycloak secret generic keycloak-tlscert --from-file=tls.crt=$(git rev-parse --show-toplevel)/scripts/mtsi-bigbang-dev-fullchain.pem
+kubectl create -n keycloak secret generic keycloak-tlskey --from-file=tls.key=$(git rev-parse --show-toplevel)/$key
+kubectl create -n keycloak secret generic keycloak-tlscert --from-file=tls.crt=$(git rev-parse --show-toplevel)/$cert
 
 # restart keycloak
 kubectl rollout restart statefulset -n keycloak keycloak
